b345bb0cb8a2aaa87d3b3bf19dca1c099106b62e
Peter Jin <peterhaijin@gmail.com>
2016-11-25 14:02:10 -0800

    Rerun the build script by checking absolute paths.

--- build.rs
+++ build.rs
@@ -4,14 +4,18 @@ extern crate walkdir;
 use walkdir::{WalkDir};
 
 use std::env;
+use std::path::{PathBuf};
 
 fn main() {
   println!("cargo:rerun-if-changed=build.rs");
-  for entry in WalkDir::new("kernels") {
+  let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+  let out_dir = env::var("OUT_DIR").unwrap();
+  let mut kernels_src_dir = PathBuf::from(manifest_dir);
+  kernels_src_dir.push("kernels");
+  for entry in WalkDir::new(&kernels_src_dir.to_str().unwrap()) {
     let entry = entry.unwrap();
     println!("cargo:rerun-if-changed={}", entry.path().display());
   }
-  let out_dir = env::var("OUT_DIR").unwrap();
   let cc = env::var("CC").unwrap_or("gcc".to_owned());
   gcc::Config::new()
     .compiler(&cc)
@@ -37,6 +41,10 @@ fn main() {
       .file("kernels/vector.c")
       .compile("libdensearray_omp_kernels.a");
   } else {
+    /*if cfg!(feature = "mkl_parallel") {
+      // XXX: For debugging to check that both features are enabled.
+      println!("cargo:rerun-if-changed=mkl_parallel_dummy");
+    }*/
     gcc::Config::new()
       .compiler("icc")
       .opt_level(2)
